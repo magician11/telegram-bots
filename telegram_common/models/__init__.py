4c87b4c5dc6ca0ba916b4e27d677181887e8ed68
magician11 <magician11@pm.me>
2025-02-24 00:25:15 +0100

    added openai model

--- telegram_common/models/__init__.py
+++ telegram_common/models/__init__.py
@@ -1,5 +1,6 @@
 from .base import ModelClient
 from .ollama import OllamaClient
 from .deepseek import DeepSeekClient
+from .openai import OpenAIClient
 
-__all__ = ['ModelClient', 'OllamaClient', 'DeepSeekClient']
+__all__ = ['ModelClient', 'OllamaClient', 'DeepSeekClient', 'OpenAIClient']
